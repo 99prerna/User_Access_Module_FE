f3a20740f29743e2bee71882ddce804cd966b17c
Nikhil Joshi <nikhiljoshi7712@gmail.com>
2024-10-17 13:24:07 +0530

    updated the login nevigation to admin and user dashboard uses of sessionStorage to validate the role bases access

---
+++ src/AuthHook.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const authHook = () => {
+  function getAuth() {
+    const token = sessionStorage.getItem("isAuth");
+    return token === "true";
+  }
+  const [isAuth, setAuth] = useState(getAuth());
+  // 1. Manage Authentication.
+  function saveAuth(isauth) {
+    sessionStorage.setItem("isAuth", isauth);
+    setAuth(isauth);
+  }
+  // 2. Manage Role.
+
+  function getAdmin() {
+    const token = sessionStorage.getItem("isAdmin");
+    return token === "true";
+  }
+  const [isAdmin, setAdmin] = useState(getAdmin());
+  // 1. Manage Authentication.
+  function saveAdmin(isadmin) {
+    sessionStorage.setItem("isAdmin", isadmin);
+    setAdmin(isadmin);
+  }
+
+  return { isAuth, saveAuth, isAdmin, saveAdmin };
+};
+
+export default authHook;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import "./login.css"; // Ensure this path is correct
 
+import authHook from "../AuthHook";
 function Login() {
   const [formData, setFormData] = useState({ username: "", password: "" });
   const navigate = useNavigate(); // Initialize useNavigate
+
+  const { saveAuth, saveAdmin, isAdmin } = authHook();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -15,6 +18,7 @@ function Login() {
     e.preventDefault();
     const response = await fetch("http://localhost:5055/loginUser", {
       method: "POST",
+      credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
@@ -22,15 +26,16 @@ function Login() {
     });
 
     const data = await response.json();
-    console.log("Response Data:", data);
 
-    if (data.success) {
-      console.log("Redirecting to:", data.redirectUrl);
-      navigate(data.redirectUrl); // Redirect to the dashboard
-    } else {
-      // Handle the error (e.g., show an error message)
-      alert(data.message);
+    if (!data.status) {
+      alert("Login failed");
+      return;
     }
+
+    saveAuth(true); // Save auth status
+    saveAdmin(data.role === "admin");
+
+    navigate(isAdmin ? "/damin/Dashboard" : "/user/Dashboard");
 
     setFormData({ username: "", password: "" });
   }
